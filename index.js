be3c277e8ca20f52c4589f65702668e23b684ca7
raaam02 <kingk9869991@gmail.com>
2024-06-16 02:00:16 +0530

    counter time button after click

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let btn = document.getElementById("btn");
 btn.onclick = () => {
-  for (let index = 10; index >= 1; index--) {
+  for (let index = 10; index >= 0; index--) {
     setTimeout(() => {
       btn.innerHTML = index;
+      console.log(index);
     }, (10 - index) * 1000);
-    console.log(index);
   }
+
+  setTimeout(() => {
+    btn.style.backgroundColor = "red";
+    btn.innerHTML = "Hello....";
+  }, 10000);
 };
