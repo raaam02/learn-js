005667ec82842eb8bb69a5292f1ea00490200f43
raaam02 <kingk9869991@gmail.com>
2024-06-17 15:32:47 +0530

    object condition loop in js

---
+++ Learning/objects loops arrays functions/script.js
@@ -0,0 +1,47 @@
+let user = {
+  uName: "Ram",
+  follow: true,
+  posts: 16,
+  followers: 569000,
+  following: 4,
+  Name: "Ram Prajapati",
+  Bio: "BSc IT graduate | IT Professional",
+};
+console.log(user);
+
+//Give Grades on the basis of marks
+
+let num = 90;
+
+if (num <= 100 && num >= 80) {
+  alert("A");
+} else if (num <= 89 && num >= 70) {
+  alert("B");
+} else if (num <= 69 && num >= 60) {
+  alert("C");
+} else if (num <= 59 && num >= 50) {
+  alert("D");
+} else {
+  alert("F");
+}
+
+// Print all even number from 0 to 100
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+// Guess number game
+
+let randomNum = Math.floor(Math.random() * 100);
+let guessNum = prompt("Guess the Number betweem 0-100");
+while (guessNum != randomNum) {
+  if (guessNum > randomNum) {
+    guessNum = prompt("Number was greater please try again...");
+  } else {
+    guessNum = prompt("Number was smaller please try again...");
+  }
+}
+alert("Congrats you Winnnnnn!!!! \n" + "my number was " + randomNum);
