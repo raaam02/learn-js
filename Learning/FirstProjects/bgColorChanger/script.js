72930315c1d62acc7245d9867c1b6a10cc841e35
raaam02 <kingk9869991@gmail.com>
2024-06-21 11:48:55 +0530

    another way of code

--- Learning/FirstProjects/bgColorChanger/script.js
+++ Learning/FirstProjects/bgColorChanger/script.js
@@ -1,36 +1,60 @@
-let body = document.querySelector("body");
 let h1 = document.querySelector("h1");
 let h3 = document.querySelector("h3");
-let redbtn = document.getElementById("red");
-let greenbtn = document.getElementById("green");
-let whitebtn = document.getElementById("white");
-let blackbtn = document.getElementById("black");
+const buttons = document.querySelectorAll("button");
+const body = document.querySelector("body");
 
-redbtn.onclick = () => {
-  let bgcolor = getComputedStyle(redbtn).backgroundColor;
-  body.style.backgroundColor = bgcolor;
-  h1.style.color = "";
-  h3.style.color = "";
-  blackbtn.style.borderColor = "";
-};
-greenbtn.onclick = () => {
-  let bgcolor = getComputedStyle(greenbtn).backgroundColor;
-  body.style.backgroundColor = bgcolor;
-  h1.style.color = "";
-  h3.style.color = "";
-  blackbtn.style.borderColor = "";
-};
-whitebtn.onclick = () => {
-  let bgcolor = getComputedStyle(whitebtn).backgroundColor;
-  body.style.backgroundColor = bgcolor;
-  h1.style.color = "";
-  h3.style.color = "";
-  blackbtn.style.borderColor = "";
-};
-blackbtn.onclick = () => {
-  let bgcolor = getComputedStyle(blackbtn).backgroundColor;
-  body.style.backgroundColor = bgcolor;
-  h1.style.color = "white";
-  h3.style.color = "white";
-  blackbtn.style.borderColor = "white";
-};
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.id === "red") {
+      body.id = btn.id;
+      h1.style.color = "";
+      h3.style.color = "";
+      btn.style.borderColor = "";
+    }
+    if (e.target.id === "green") {
+      body.id = btn.id;
+      h1.style.color = "";
+      h3.style.color = "";
+      btn.style.borderColor = "";
+    }
+    if (e.target.id === "white") {
+      body.id = btn.id;
+      h1.style.color = "";
+      h3.style.color = "";
+      btn.style.borderColor = "";
+    }
+    if (e.target.id === "black") {
+      body.id = btn.id;
+      h1.style.color = "white";
+      h3.style.color = "white";
+      btn.style.borderColor = "white";
+    }
+  });
+});
+
+// redbtn.onclick = () => {
+//   let bgcolor = getComputedStyle(redbtn).backgroundColor;
+//   body.style.backgroundColor = bgcolor;
+//   h1.style.color = "";
+//   h3.style.color = "";
+//   blackbtn.style.borderColor = "";
+// };
+// greenbtn.onclick = () => {
+//   let bgcolor = getComputedStyle(greenbtn).backgroundColor;
+//   body.style.backgroundColor = bgcolor;
+//   h1.style.color = "";
+//   h3.style.color = "";
+//   blackbtn.style.borderColor = "";
+// };
+// whitebtn.onclick = () => {
+//   let bgcolor = getComputedStyle(whitebtn).backgroundColor;
+//   body.style.backgroundColor = bgcolor;
+//   h1.style.color = "";
+//   h3.style.color = "";
+//   blackbtn.style.borderColor = "";
+// };
+// blackbtn.onclick = () => {
+//   let bgcolor = getComputedStyle(blackbtn).backgroundColor;
+//   body.style.backgroundColor = bgcolor;
+
+// };
